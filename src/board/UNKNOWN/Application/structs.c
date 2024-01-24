edeed5863ace0da681be91b164c98843205ad4cf
Intes1k <yubko.tema@gmail.com>
2024-01-24 17:20:51 +0300

    да

--- src/board/UNKNOWN/Application/structs.c
+++ src/board/UNKNOWN/Application/structs.c
@@ -6,16 +6,18 @@
  */
 #include <string.h>
 #include <stdio.h>
-
+// 18
 typedef struct{
 	uint8_t flag;
-	int16_t accl[3];
-	int16_t gyro[3];
-	int16_t mag[3];
+	int16_t accl[3]; //6
+	int16_t gyro[3]; // 6
+	int16_t mag[3];// 6
 }pack1_t;
+//11
 typedef struct{
-	uint8_t flag;
-	double bmp_temp;
-	double bmp_press;
-	double bmp_humidity;
+	uint8_t flag; // 1
+	int16_t bmp_temp; // 2
+	uint32_t bmp_press; //4
+	int16_t bmp_humidity; // 2
+	float bme_height; // 2
 }pack2_t;
