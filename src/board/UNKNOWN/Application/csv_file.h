a8c9e3aa1ad554d9cddb5ac7b82b78ed00802484
Intes1k <yubko.tema@gmail.com>
2024-02-03 20:22:32 +0300

    тат конкретный +вайб

--- src/board/UNKNOWN/Application/csv_file.h
+++ src/board/UNKNOWN/Application/csv_file.h
@@ -16,4 +16,8 @@ uint16_t sd_parse_to_bytes_pack1(char *buffer, pack1_t *pack1);
 
 uint16_t sd_parse_to_bytes_pack2(char *buffer, pack2_t *pack2);
 
+uint16_t sd_parse_to_bytes_pack3(char *buffer, pack3_t *pack3);
+
+uint16_t sd_parse_to_bytes_quaterneon(char *buffer, packq_t *q);
+
 #endif /* CSV_FILE_H_ */
