9d4a54a3e97e62349231d60cc6a6af08c04b8b30
Mary662 <m89163214957@inbox.ru>
2024-02-17 16:01:58 +0300

    начало наземки

---
+++ src/gcs/source/aaa.py
@@ -0,0 +1,42 @@
+from PyQt5 import QtWidgets
+
+
+
+
+
+
+
+
+
+class CentralWidget(QtWidgets.QWidget):
+	def __init__(self):
+		super(CentralWidget, self).__init__()
+		self.textEdit = QtWidgets.QTextEdit()
+		self.button = QtWidgets.QPushButton()
+		self.layoutube = QtWidgets.QVBoxLayout()
+		self.setLayout(self.layoutube)
+		self.layoutube.addWidget(self.textEdit)
+		
+
+		self.button2 = QtWidgets.QPushButton()
+		self.layoutube2 = QtWidgets.QHBoxLayout()
+		self.layoutube.addLayout(self.layoutube2)
+		self.layoutube2.addWidget(self.button)
+		self.layoutube2.addWidget(self.button2)
+
+
+
+class MainWindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.setWindowTitle("ROMA GA Y")
+		self.resize(1488, 322)
+		self.move_center()
+		self.c_widget = CentralWidget();
+		self.setCentralWidget(self.c_widget) 
+
+
+	def move_center(self):
+		frame = self.frameGeometry()
+		frame.moveCenter(QtWidgets.QDesktopWidget().availableGeometry().center())
+		self.move(frame.topLeft())
