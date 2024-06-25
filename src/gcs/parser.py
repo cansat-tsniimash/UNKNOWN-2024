bcbd6ffeaff1e43a4888e7affe1dd3491b3eb0fe
Mary662 <m89163214957@inbox.ru>
2024-06-25 19:54:32 +0300

    я стоко всего сделал ну стоко это просто жесть у нас теперь работает парсер... ну не то что бы он и раньше не работал, но до этого он не считал контрольную сумму пакетов, а сейчас он её считает. Наверное это очень важно, но мы впринципе и без этого нормально разбирали пакеты с наземки, хотя сейчас он ещё и не записывает пакеты с какими-то странными данными, так что этотоже очень круто. Ну я думаю надо ещё чёто сделать с наземкой, потому что она эти странные пакеты и принимает, и записывает на файлик, но я не знаю что с этим можно сделать. Ещё я поменял местами в gcs.py и в парсере первый пакет и второй (нет это не какой-то там пранк или троллинг, просто до этого у нас пакеты шли в таком порядке: второй, первый, третий, четвёртый), поэтому сейчас они идут в нормальном порядке, я не знаю кто до этого допустил такой бред, как поменяные местами пакеты, но хотя бы сейчас с ними всё хорошо.(надеюсь это никак не повлияет на код(ну вроде бы сейчас оба кода ещё нормально работают и я не заметил никаких изменений(ну их и не должно быть, я только поменял местами разбор пакетов в коде)(хоть я и запускал только парсер, но мне кажется что и в гкс всё должно быть хорошо))). Ещё у нас теперь есть какие-то файлики в папочках, они мне ещё пригодятся для тестов(наверное) ну короче их не удаляйте.

--- src/gcs/parser.py
+++ src/gcs/parser.py
@@ -7,20 +7,11 @@ import socket
 import numpy as np
 import ctypes
 
-HowMuchMusor=0
+HowMuchMusor,HowMuchDefective=0,0
 
-#def MostEfficiantAndTruthlyBestChecksummCalculation(data):
-   # crc = 0xFFFF
-    #buf,dlina=0,len(data)-1
-    #while dlina >= 0:
-    #    crc ^= data[buf] << 8
-    #    for i in range(8):
-    #        if crc == crc & 0x8000:
-    #            (crc << 1) ^ 0x1021
-    #        else:
-    #            crc << 1
-    #    dlina,buf=dlina-1,buf+1
-    #return crc
+def DoNothing(variable):
+    return variable
+
 def crc16(data : bytearray, offset=0, length=-1):
     if length < 0:
         length = len(data)
@@ -32,12 +23,13 @@ def crc16(data : bytearray, offset=0, length=-1):
     for i in range(0, length):
         crc ^= data[offset + i] << 8
         for j in range(0, 8):
-            if (crc & 0x8000) == crc:
-                (crc << 1) ^ 0x1021
+            if (crc & 0x8000) > 0:
+                crc = (crc << 1) ^ 0x1021
             else:
-                crc << 1
+                crc = crc << 1
+        crc = crc & 0xFFFF
 
-    return crc 
+    return crc & 0xFFFF
 
 def generate_csv_name(text):
     now = datetime.datetime.utcnow().replace(microsecond=0)
@@ -79,115 +71,138 @@ if __name__ == '__main__':
             if len(data) < 1:
                 print("Наши данные иссякли")
                 print("нашёл", HowMuchMusor, "лишних байтов")
+                print("странных пакетов:", HowMuchDefective)
                 exit()
-            if data[0] == 187:
-                data_sir = f.read(25)
+            else:
+                one = DoNothing("one")
+            if data[0] == 170:
+                data_sir = f.read(26)
+                data_sir = bytes(range(170,171)) + data_sir
                 data = np.frombuffer(data_sir, dtype=np.uint8)
-                print(crc16(data))
+    
+                print("==== Пакет тип 1 ====")
+                unpack_data = struct.unpack("<BHIhhhhhhhhhH", data)
+                print ("Number", unpack_data[1])
+                print ("Time_ms", unpack_data[2])
+                print ("Accelerometer x", unpack_data[3]/1000)
+                print ("Accelerometer y", unpack_data[4]/1000)
+                print ("Accelerometer z", unpack_data[5]/1000)
+                print ("Gyroscope x", unpack_data[6]/1000)
+                print ("Gyroscope y", unpack_data[7]/1000)
+                print ("Gyroscope z", unpack_data[8]/1000)
+                print ("Magnetometer x", unpack_data[9]/1000)
+                print ("Magnetometer y", unpack_data[10]/1000)
+                print ("Magnetometer z", unpack_data[11]/1000)
+                print ("crc", unpack_data[12])
+                print(crc16(data[:25]))
+                # print ("Number", unpack_data[2])
+                # print ("Time", unpack_data[1])
+                
+                if crc16(data[:25])==unpack_data[12]:
+                    for i in range(1,13):
+                        f1.write(str(unpack_data[i]))
+                        f1.write(";")
+                    f1.write('\n')
+                    f1.flush()
+                else:
+                    print("Не совпадает контрольная сумма")
+                    HowMuchDefective+=1
+                print ('\n')
+
+            elif data[0] == 187:
+                data_sir = f.read(25)
+                data_sir = bytes(range(187,188)) + data_sir
+                data = np.frombuffer(data_sir, dtype=np.uint8)
+
                 print("==== Пакет тип 2 ====")
-                unpack_data = struct.unpack("<HIhIhffBH", data)
-                #pack2 = ThePacketFlaggedWithNumberTwo(unpack_data[])
-                print ("Number", unpack_data[0])
-                print ("Time_ms", unpack_data[1])
-                print ("Temperature BME", unpack_data[2]/100)
-                print ("Pressure", unpack_data[3])
-                print ("Humidity", unpack_data[4])
-                print ("Height", unpack_data[5])
-                print ("Lux", unpack_data[6])
-                print ("State", unpack_data[7])
-                print ("crc", unpack_data[8])
+                unpack_data = struct.unpack("<BHIhIhffBH", data)
+                print ("Number", unpack_data[1])
+                print ("Time_ms", unpack_data[2])
+                print ("Temperature BME", unpack_data[3]/100)
+                print ("Pressure", unpack_data[4])
+                print ("Humidity", unpack_data[5])
+                print ("Height", unpack_data[6])
+                print ("Lux", unpack_data[7])
+                print ("State", unpack_data[8])
+                print ("crc", unpack_data[9])
+                print(crc16(data[:24]))
                 # print ("Bus voltage", unpack_data[7]/1000)
                 # print ("Current", unpack_data[6]/1000)
                 # print ("Number", unpack_data[2])
                 # print ("Photo", unpack_data[9]/100)
                 # print ("State", unpack_data[8])
                 # print ("Time", unpack_data[1])
+                
+                if crc16(data[:24])==unpack_data[9]:
+                    for i in range(1,10):
+                        f2.write(str(unpack_data[i]))
+                        f2.write(";")
+                    f2.write('\n')
+                    f2.flush()
+                else:
+                    print("Не совпадает контрольная сумма")
+                    HowMuchDefective+=1
                 print ('\n')
 
-                for i in range(9):
-                    f2.write(str(unpack_data[i]))
-                    f2.write(";")
-                f2.write('\n')
-                f2.flush()
-                
-            elif data[0] == 170:
-                data_sir = f.read(26)
-                data = np.frombuffer(data_sir, dtype=np.uint8)
-                print(crc16(data))
-                print("==== Пакет тип 1 ====")
-                unpack_data = struct.unpack("<HIhhhhhhhhhH", data)
-                print ("Number", unpack_data[0])
-                print ("Time_ms", unpack_data[1])
-                print ("Accelerometer x", unpack_data[2]/1000)
-                print ("Accelerometer y", unpack_data[3]/1000)
-                print ("Accelerometer z", unpack_data[4]/1000)
-                print ("Gyroscope x", unpack_data[5]/1000)
-                print ("Gyroscope y", unpack_data[6]/1000)
-                print ("Gyroscope z", unpack_data[7]/1000)
-                print ("Magnetometer x", unpack_data[8]/1000)
-                print ("Magnetometer y", unpack_data[9]/1000)
-                print ("Magnetometer z", unpack_data[10]/1000)
-                print ("crc", unpack_data[11])
-                # print ("Number", unpack_data[2])
-                # print ("Time", unpack_data[1])
-                print ('\n')
-
-                for i in range(12):
-                    f1.write(str(unpack_data[i]))
-                    f1.write(";")
-                f1.write('\n')
-                f1.flush()
-                
             elif data[0] == 204:
                  data_sir = f.read(30)
+                 data_sir = bytes(range(204,205)) + data_sir
                  data = np.frombuffer(data_sir, dtype=np.uint8)
-                 print(MostEfficiantAndTruthlyBestChecksummCalculation(data))
                  print("==== Пакет тип 3 ====")
-                 unpack_data = struct.unpack("<HIhfffLLH", data)
-                 print ("Number", unpack_data[0])
-                 print ("Time_ms", unpack_data[1])
-                 print ("Fix", unpack_data[2])
-                 print ("Latitude", unpack_data[3])
-                 print ("Longitude", unpack_data[4])
-                 print ("Height", unpack_data[5])
-                 print ("Time, s", unpack_data[6])
-                 print ("Time, mks", unpack_data[7])
-                 print ("crc", unpack_data[8])
+                 unpack_data = struct.unpack("<BHIhfffLLH", data)
+                 print ("Number", unpack_data[1])
+                 print ("Time_ms", unpack_data[2])
+                 print ("Fix", unpack_data[3])
+                 print ("Latitude", unpack_data[4])
+                 print ("Longitude", unpack_data[5])
+                 print ("Height", unpack_data[6])
+                 print ("Time, s", unpack_data[7])
+                 print ("Time, mks", unpack_data[8])
+                 print ("crc", unpack_data[9])
+                 print(crc16(data[:29]))
                  #print ("Number", unpack_data[2])
                  #print ("Time", unpack_data[1])
+                 
+                 if crc16(data[:29])==unpack_data[9]:
+                    for i in range(1,10):
+                        f3.write(str(unpack_data[i]))
+                        f3.write(";")   
+                    f3.write('\n')
+                    f3.flush()
+                 else:
+                    print("Не совпадает контрольная сумма")
+                    HowMuchDefective+=1
                  print ('\n')
 
-                 for i in range(9):
-                     f3.write(str(unpack_data[i]))
-                     f3.write(";")   
-                 f3.write('\n')
-                 f3.flush()
-                 
             elif data[0] == 255:
                  data_sir = f.read(28)
+                 data_sir = bytes(range(255,256)) + data_sir
                  data = np.frombuffer(data_sir, dtype=np.uint8)
-                 print(MostEfficiantAndTruthlyBestChecksummCalculation(data))
                  print("==== Пакет тип 4 ====")
-                 unpack_data = struct.unpack("<HIfffffH", data)
-                 print ("Number", unpack_data[0])
-                 print ("Time_ms", unpack_data[1])
-                 print ("Q1", unpack_data[3])
-                 print ("Q2", unpack_data[4])
-                 print ("Q3", unpack_data[5])
-                 print ("Q4", unpack_data[6])
-                 print ("Time", unpack_data[2])
-                 print ("crc", unpack_data[7])
+                 unpack_data = struct.unpack("<BHIfffffH", data)
+                 print ("Number", unpack_data[1])
+                 print ("Time_ms", unpack_data[2])
+                 print ("Q1", unpack_data[4])
+                 print ("Q2", unpack_data[5])
+                 print ("Q3", unpack_data[6])
+                 print ("Q4", unpack_data[7])
+                 print ("Time", unpack_data[3])
+                 print ("crc", unpack_data[8])
+                 print(crc16(data[:27]))
+                 
+                 if crc16(data[:27])==unpack_data[8]:
+                    for i in range(1,9):
+                        f4.write(str(unpack_data[i]))
+                        f4.write(";")    
+                    f4.write('\n')
+                    f4.flush()
+                 else:
+                    print("Не совпадает контрольная сумма")
+                    HowMuchDefective+=1
                  print ('\n')
-                     
-                 for i in range(8):
-                     f4.write(str(unpack_data[i]))
-                     f4.write(";")    
-                 f4.write('\n')
-                 f4.flush()
             else:
-                #print('какой-то мусор с флагом', data[0])
                 HowMuchMusor+=1
-            time.sleep(0.5)
+            #time.sleep(1)
         except Exception as e:
             print(e)
 
